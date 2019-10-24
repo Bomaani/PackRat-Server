92926fd9e162a8732863297929ae537be938ef05
Brock Boutwell <brockboutwell@gmail.com>
2019-10-24 14:29:01 -0400

    heroku deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/packrat',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/packrat',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-packrat',
 };
